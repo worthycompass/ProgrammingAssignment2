df367a9bf2bd5e343b8330b0c8646170989a8669
worthycompass <43348053+worthycompass@users.noreply.github.com>
2018-10-29 18:53:55 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2
 
-## Write a short comment describing this function
+## makeCacheMatrix() will get a matrix from the browser/ user and will set 
+## the given matrix in the variable "m" here which will reside in parent 
+## environment (using << operator)
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(m = matrix()) {
 
+  
+  inv <- NULL	 
+  
+  set <- function(tempmat) {
+    #initializing variables
+    m <<- tempmat				
+    inv <<- NULL	
+  }
+  
+  get <- function() m
+  #Setting inverse of matrix m into inv variable
+  setInverse <- function() inv <<- solve(m)
+  getInverse <- function() inv
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function gets the inverse of the matrix "inv" which it can
+## access due to the variable residing in an parent environment or cache
+## This function will return the inv or inverse of the matrix given
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m, ...) {
+        ## Return a matrix that is the inverse of 'm'
+  inv <- m$getInverse()
+  
+  if(!is.null(inv)) {
+    message("getting cached matrix")
+    return(inv)
+  }
+  matr <- m$get()
+  inv <- solve(matr, ...)
+  m$setInverse(inv)
+  inv
+  
 }
+
+
+##Example of how to call function
+
+#fn <- makeCacheMatrix()
+#fn$set(matrix(1:4, 2))
+#fn$get()
+#fn$setInverse()
+#fn$getInverse()
